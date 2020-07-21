b5039b1105c7efe48f4ee9a65a846f7ff78faec0
Nathan Freeman <NathanLFreeman@gmail.com>
2020-07-20 22:30:10 -0400

    Add basic black/white drawing and reset functionality
    Struggled to compartmentalize and nail down the coding mechanics for this project. After studying CSS grid, implemented the basics to get a variable resolution grid. Tried to figure out how to implement random color and greyscale, and they are proving difficult, though I understand the basics. Will keep working on those and update as progress is made.
    
    Did finally come up with a good recursive function to check for an acceptable integer number of rows/columns, and placed it inside the resetGrid function.

--- Etch.js
+++ Etch.js
@@ -1,19 +1,65 @@
-
-const container = document.getElementById('container');
-const newDiv = document.createElement('div');
-newDiv.classList.add('inner-square');
-container.appendChild(newDiv);
-//const num = prompt('How many rows/columns?');
-innerSquareDivs = document.getElementsByClassName('inner-square');
-/*innerSquareDivs.forEach({
-    style.width = 640/num; 
-    style.height = 640/num;
-});*/
-for (i=1; i<16*16; i++) {
-  container.appendChild(newDiv.cloneNode(true));  
+const container = document.querySelector('#container');
+let gridSquare = document.createElement('div');
+for (i=0; i<16*16; i++) {
+  container.appendChild(gridSquare.cloneNode(true));
+}
+const allGridSquares = container.children;
+for (i=0; i<16*16; i++) {
+  allGridSquares[i].classList.add('grid-square');
+  allGridSquares[i].addEventListener('mouseenter', fillBlack)
 };
 
-function fillColor(e) {
-    e.style.backgroundColor = '#000';
+function fillBlack(e) {
+  e.target.style.backgroundColor = '#000';
 }
 
+const resetBtn = document.querySelector('#reset-button');
+const bwBtn = document.querySelector('#bw-button');
+const greyscaleBtn = document.querySelector('#greyscale-button');
+const randomizerBtn = document.querySelector('#random-color-button');
+
+resetBtn.addEventListener('click', resetGrid);
+
+
+function resetGrid() {
+  container.innerHTML = "";
+  let num = prompt('How many squares on each side of the grid? More squares equals finer control over the sketch.', '16');
+  checkNum();
+  function checkNum() {
+    if (num<1||num>256) {
+      num = prompt('Please choose an integer greater than 1 but no more than 256.');
+      checkNum();
+    }
+  }  
+  const sq = num * num;
+  container.style.gridTemplateRows = `repeat(${num}, auto)`;
+  container.style.gridTemplateColumns = `repeat(${num}, auto)`;  
+  for (i=0; i<sq; i++) {
+    container.appendChild(gridSquare.cloneNode(true));
+  }
+  for (i=0; i<sq; i++) {
+    allGridSquares[i].classList.add('grid-square');
+    allGridSquares[i].addEventListener('mouseenter', fillBlack);
+  };
+}
+/*
+for (i=0; i<sq; i++) {
+  allGridSquares[i].addEventListener('mouseenter', ;
+}
+
+function fillColor(e) {
+  e.target.style.backgroundColor = rgb(0,0,0);
+}
+function getRandomColor() {
+  let r = Math.floor(Math.random()*256);
+  let g = Math.floor(Math.random()*256);
+  let b = Math.floor(Math.random()*256);
+  const randomColor = "rgb("+r+","+g+","+b+")";
+  return randomColor;
+  /*When grid-item has a mouseenter event, fill it with a random color. Then remove event listener so that it cannot be changed from that color.
+  grid-item.addEventListener('mouseenter', fill with appropriate color);
+}
+
+function makeGreyscale() {
+/*if grid-item is empty, or not filled with any color yet, then begin filling at 10% opacity/saturation/hue? If grid-item not empty, add 10% opacity/saturation/hue to existing rgba color value.
+}*/
