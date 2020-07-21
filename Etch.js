55c890227d66aab1d9a97ad5a5a63d624712f6c6
Nathan Freeman <NathanLFreeman@gmail.com>
2020-07-21 01:07:02 -0400

    Add Random, Black, and Greyscale functionality
    Add the buttons for each mode of drawing and the functions that control each mode. Some issues remain when going from some modes to others. Upon page load, all buttons work correctly. But greyscale has some issues when moving to other modes after using it without resetting. Opacity is the challenge. For some reason it doesn't reset opacity correctly. Ran out of time to fix it today, but app largely works correctly. Lots of progress.

--- Etch.js
+++ Etch.js
@@ -8,26 +8,26 @@ for (i=0; i<16*16; i++) {
   allGridSquares[i].classList.add('grid-square');
   allGridSquares[i].addEventListener('mouseenter', fillBlack)
 };
-
 function fillBlack(e) {
   e.target.style.backgroundColor = '#000';
 }
-
 const resetBtn = document.querySelector('#reset-button');
 const bwBtn = document.querySelector('#bw-button');
 const greyscaleBtn = document.querySelector('#greyscale-button');
 const randomizerBtn = document.querySelector('#random-color-button');
 
 resetBtn.addEventListener('click', resetGrid);
-
+bwBtn.addEventListener('click', changeToBlack);
+greyscaleBtn.addEventListener('click', makeGreyscale);
+randomizerBtn.addEventListener('click', randomizeColor);
 
 function resetGrid() {
   container.innerHTML = "";
   let num = prompt('How many squares on each side of the grid? More squares equals finer control over the sketch.', '16');
   checkNum();
   function checkNum() {
-    if (num<1||num>256) {
-      num = prompt('Please choose an integer greater than 1 but no more than 256.');
+    if (num<1||num>128) {
+      num = prompt('Please choose an integer greater than 1 but no more than 128.');
       checkNum();
     }
   }  
@@ -42,24 +42,39 @@ function resetGrid() {
     allGridSquares[i].addEventListener('mouseenter', fillBlack);
   };
 }
-/*
-for (i=0; i<sq; i++) {
-  allGridSquares[i].addEventListener('mouseenter', ;
+function changeToBlack() {
+  for (i=0; i<allGridSquares.length; i++) {
+    allGridSquares[i].addEventListener('mouseenter', backToBlack);
+  }
+  function backToBlack(e) {
+    e.target.style.opacity = 1;
+    e.target.style.backgroundColor = '#000';
+  }
 }
-
-function fillColor(e) {
-  e.target.style.backgroundColor = rgb(0,0,0);
+function randomizeColor() {
+  for (i=0; i<allGridSquares.length; i++) {
+    allGridSquares[i].addEventListener('mouseenter', getRandomColor);
+  };
+  function getRandomColor(e) {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    const randomColor = "rgb("+r+","+g+","+b+")";
+    e.target.style.backgroundColor = randomColor;
+  }  
 }
-function getRandomColor() {
-  let r = Math.floor(Math.random()*256);
-  let g = Math.floor(Math.random()*256);
-  let b = Math.floor(Math.random()*256);
-  const randomColor = "rgb("+r+","+g+","+b+")";
-  return randomColor;
-  /*When grid-item has a mouseenter event, fill it with a random color. Then remove event listener so that it cannot be changed from that color.
-  grid-item.addEventListener('mouseenter', fill with appropriate color);
-}
-
 function makeGreyscale() {
-/*if grid-item is empty, or not filled with any color yet, then begin filling at 10% opacity/saturation/hue? If grid-item not empty, add 10% opacity/saturation/hue to existing rgba color value.
-}*/
+  function getGreyscaleColor(e) {
+     console.log(e.target.style.opacity);
+    if (e.target.style.backgroundColor !== 'rgb(0, 0, 0)') {
+      e.target.style.backgroundColor = '#000';
+      e.target.style.opacity = 0.1;
+    } else {
+      e.target.style.opacity = Number(e.target.style.opacity) + 0.1;
+      console.log(e.target.style.opacity);
+    }
+  }
+  for (i=0; i<allGridSquares.length; i++) {
+    allGridSquares[i].addEventListener('mouseenter', getGreyscaleColor);
+  }
+}
