0e952e7044f2251f879e910298b0410898ccdcd1
Nathan Freeman <NathanLFreeman@gmail.com>
2020-07-22 00:33:56 -0400

    Update script, solve functionality issues
    Was confounded by why event listeners would carry over and cause functionality issues. Greyscale was the big culprit. Determining how to handle opacity (or as turned out to be better, alpha values) took up the majority of my study and brain space today. But I may have a working solution here. Need to add another else clause to handle circumstance where alphaValue is 1.0 and ends up reverting to 0.1. But that hopefully will not be too difficult.

--- Etch.js
+++ Etch.js
@@ -1,21 +1,27 @@
+//create initial grid
 const container = document.querySelector('#container');
 let gridSquare = document.createElement('div');
 for (i=0; i<16*16; i++) {
   container.appendChild(gridSquare.cloneNode(true));
 }
 const allGridSquares = container.children;
-for (i=0; i<16*16; i++) {
-  allGridSquares[i].classList.add('grid-square');
-  allGridSquares[i].addEventListener('mouseenter', fillBlack)
-};
-function fillBlack(e) {
-  e.target.style.backgroundColor = '#000';
+function defaultBehavior() {  
+  for (i=0; i<16*16; i++) {
+    allGridSquares[i].classList.add('grid-square');
+    allGridSquares[i].addEventListener('mouseenter', fillBlack);
+  }
 }
+//set default behavior
+function fillBlack(e) {
+  e.target.style.backgroundColor = 'rgb(0, 0, 0)';
+}
+defaultBehavior();
+//store button variables
 const resetBtn = document.querySelector('#reset-button');
 const bwBtn = document.querySelector('#bw-button');
 const greyscaleBtn = document.querySelector('#greyscale-button');
 const randomizerBtn = document.querySelector('#random-color-button');
-
+//what to run when each button clicked
 resetBtn.addEventListener('click', resetGrid);
 bwBtn.addEventListener('click', changeToBlack);
 greyscaleBtn.addEventListener('click', makeGreyscale);
@@ -43,15 +49,19 @@ function resetGrid() {
   };
 }
 function changeToBlack() {
+  //the following line of code removes existing eventListeners from container
+  container.innerHTML = container.innerHTML;
   for (i=0; i<allGridSquares.length; i++) {
+    allGridSquares[i].innerHTML = "";
     allGridSquares[i].addEventListener('mouseenter', backToBlack);
   }
   function backToBlack(e) {
-    e.target.style.opacity = 1;
-    e.target.style.backgroundColor = '#000';
+    e.target.style.backgroundColor = 'rgb(0, 0, 0)';
   }
 }
 function randomizeColor() {
+  //the following line of code removes existing eventListeners from container
+  container.innerHTML = container.innerHTML;
   for (i=0; i<allGridSquares.length; i++) {
     allGridSquares[i].addEventListener('mouseenter', getRandomColor);
   };
@@ -61,20 +71,52 @@ function randomizeColor() {
     let b = Math.floor(Math.random()*256);
     const randomColor = "rgb("+r+","+g+","+b+")";
     e.target.style.backgroundColor = randomColor;
-    e.target.style.opacity = 1;
   }  
 }
 function makeGreyscale() {
-  function getGreyscaleColor(e) {
-    console.log(e.target.style.backgroundColor);
-    if (!(e.target.style.backgroundColor == 'rgb(0, 0, 0)')) {
-      e.target.style.backgroundColor = '#000';
-      e.target.style.opacity = 0.1;
-    } else {
-      e.target.style.opacity = Number(e.target.style.opacity) + 0.1;
-    }
-  }
+  //the following line of code removes existing eventListeners from container
+  container.innerHTML = container.innerHTML;
   for (i=0; i<allGridSquares.length; i++) {
     allGridSquares[i].addEventListener('mouseenter', getGreyscaleColor);
   }
+  function getGreyscaleColor(e) {
+    const blackRGB = "0, 0, 0";
+    const startValue = "rgba(0, 0, 0, 0.1)";
+    //console.log("rgba? "+e.target.style.backgroundColor.search('rgba'));
+    if(e.target.style.backgroundColor.search('rgba') === -1) {
+      e.target.style.backgroundColor = startValue;
+      //console.log(e.target.style.backgroundColor);
+    } else {
+      let alphaValue = e.target.style.backgroundColor.slice(14, 17);
+      console.log(alphaValue);
+      if (alphaValue > 0 && alphaValue < 1) {
+        alphaValue = Number(alphaValue) + 0.1;
+        console.log(alphaValue);
+        e.target.style.backgroundColor = `rgb(${blackRGB}, ${alphaValue})`;
+      }
+    }
+      /*else if (alphaValue > 0 && alphaValue < 1) {
+      console.log(alphaValue + 0.1);
+      alphaValue += 0.1;
+      console.log(alphaValue);
+      e.target.style.backgroundColor = `rgba(${blackRGB}, ${alphaValue})`;
+      
+    } else if(e.target.style.alphaValue === '1' && e.target.style.backgroundColor.search('rgba') === 3 ) {
+      //do nothing (grid square should already be at alphaValue of 1; otherwise, square would start over at alphaValue 0.1)
+      ;
+      else {
+        console.log("An error may have occurred.");
+    }*/
+  }
+  /*console.log(e.target.style.backgroundColor);
+  if (e.target.style.backgroundColor == 'rgb(0, 0, 0)') {
+    console.log(e.target.style.alphaValue);
+    e.target.style.alphaValue = Number(e.target.style.alphaValue) + 0.1;
+    console.log(e.target.style.alphaValue);
+  } else {
+    e.target.style.backgroundColor = 'rgb(0, 0, 0)';
+    e.target.style.alphaValue = 0.1;
+  }*/
+  
+  
 }
